c0ae32c3a1b58377894e8682f99a4085347b6bcf
Aleks_Fy <ptolome1984@gmail.com>
2025-12-07 18:26:04 +0300

    fix: added folder public

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: './',
     server: {
         port: 3000,
         open: true
     },
+
     build: {
-        outDir: 'dist',
-        assetsDir: 'assets'
-    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+     rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'images';
+                    } else if (/woff2?|eot|ttf|otf/i.test(extType)) {
+                        extType = 'fonts';
+                    }
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js'
+            }
+        }
+  },
     css: {
     preprocessorOptions: {
       scss: {
